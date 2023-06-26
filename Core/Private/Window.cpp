58341a3ccb576e50266de5308e7d3a2cc5f832d6
Leandro Peres <leandroperes@protonmail.com>
2023-06-26 08:55:07 -0300

    Keyborad Input

--- Core/Private/Window.cpp
+++ Core/Private/Window.cpp
@@ -5,6 +5,11 @@ Window::Window()
 	width = 800;
 	height = 600;
 	aspectRatio = (GLfloat)width / (GLfloat)height;
+
+	for (auto &key : keys)
+	{
+		key = false;
+	}
 }
 
 Window::Window(GLint windowWidth, GLint windowHeight)
@@ -23,13 +28,13 @@ int Window::Initialize()
 		return 1;
 	}
 
-    // Setup GLFW
-    // OpenGL 3.3 (MAJOR.MINOR)
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                 	// Set major version to 3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                 	// Set minor version to 3
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 	// Use core profile (no backwards compatibility)
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           	// Allow forward compatibility
-	glfwWindowHint(GLFW_SAMPLES, 8);								// 8x antialiasing
+	// Setup GLFW
+	// OpenGL 3.3 (MAJOR.MINOR)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);				   // Set major version to 3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);				   // Set minor version to 3
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Use core profile (no backwards compatibility)
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);		   // Allow forward compatibility
+	glfwWindowHint(GLFW_SAMPLES, 8);							   // 8x antialiasing
 
 	// Enable vsync
 	glfwSwapInterval(1);
@@ -61,16 +66,40 @@ int Window::Initialize()
 		return 1;
 	}
 
-    // Enable depth
+	// Enable depth
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_MULTISAMPLE);
 
 	// Create Viewport
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
+	// Set the window to the current object
+	glfwSetWindowUserPointer(mainWindow, this);
+
+	// Handle key presses and releases
+	glfwSetKeyCallback(mainWindow, handleKeys);
+
 	return 0;
 }
 
+void Window::handleKeys(GLFWwindow *window, int key, int code, int action, int mode)
+{
+	// Get the window
+	Window *theWindow = static_cast<Window *>(glfwGetWindowUserPointer(window));
+
+	// Close the window if the escape key is pressed
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		theWindow->setShouldClose(true);
+		return;
+	}
+
+	// Check if the key is within the range of the array
+	if (key < 0 || key >= 1024) return;
+
+	// Set the key to true if pressed, false if released
+	theWindow->keys[key] = action != GLFW_RELEASE;
+}
 
 Window::~Window()
 {
--- Core/Public/Window.h
+++ Core/Public/Window.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "stdio.h"
+#include <stdio.h>
+#include <array>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -15,24 +16,37 @@ public:
     int Initialize();
 
     // Getters
-     GLint getBufferWidth() { return bufferWidth; }
-     GLint getBufferHeight() { return bufferHeight; }
-     GLfloat getAspectRatio() { return aspectRatio; }
-     bool getShouldClose() { return glfwWindowShouldClose(mainWindow); }
+    inline GLint getBufferWidth() { return bufferWidth; }
+    inline GLint getBufferHeight() { return bufferHeight; }
+    inline GLfloat getAspectRatio() { return aspectRatio; }
+    inline bool getShouldClose() { return glfwWindowShouldClose(mainWindow); }
+    inline GLFWwindow* getWindow() { return mainWindow; }
 
     // Setters
-     void setShouldClose(bool shouldClose) { glfwSetWindowShouldClose(mainWindow, shouldClose); }
-     void setWindowLabel(const char* label) { glfwSetWindowTitle(mainWindow, label); }
+    inline void setShouldClose(bool shouldClose) { glfwSetWindowShouldClose(mainWindow, shouldClose ? GL_TRUE : GL_FALSE); }
+    inline void setWindowLabel(const char* label) { glfwSetWindowTitle(mainWindow, label); }
 
     // Other
-    void swapBuffers() { glfwSwapBuffers(mainWindow); }
+    inline void swapBuffers() { glfwSwapBuffers(mainWindow); }
 
     ~Window();
 
 private:
-    GLFWwindow* mainWindow;
+    GLFWwindow* mainWindow = nullptr;
 
-    GLint width, height;
-    GLint bufferWidth, bufferHeight;
-    GLfloat aspectRatio;
+    GLint width = 0;
+    GLint height = 0;
+    GLint bufferWidth, bufferHeight = 0;
+    GLfloat aspectRatio = 0;
+
+    bool keys[1024]{};
+
+    /*
+    * @brief: Handles key presses and releases.
+    * @param window: The window that received the event.
+    * @param key: The keyboard key that was pressed or released.
+    * @param code: The system-specific scancode of the key.
+    * @param action: GLFW_PRESS, GLFW_RELEASE or GLFW_REPEAT.
+    */
+    static void handleKeys(GLFWwindow* window, int key, int code, int action, int mode);
 };
