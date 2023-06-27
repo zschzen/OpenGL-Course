79a956feb411351739727b93283a86346268ccbb
Leandro Peres <leandroperes@protonmail.com>
2023-06-27 18:04:52 -0300

    Proper binding order

--- Core/Private/Mesh.cpp
+++ Core/Private/Mesh.cpp
@@ -7,7 +7,7 @@ Mesh::Mesh() {}
 Mesh::Mesh(MeshData meshData)
 {
     UpdateMesh(meshData.vertices.data(), meshData.indices.data(), 
-               static_cast<unsigned int>(meshData.vertices.size() / 3), 
+               static_cast<unsigned int>(meshData.vertices.size()), 
                static_cast<unsigned int>(meshData.indices.size()));
 }
 
@@ -28,16 +28,19 @@ void Mesh::UpdateMesh(GLfloat* vertices, unsigned int* indices, unsigned int num
     glGenVertexArrays(1, &subMesh.VAO);
     glBindVertexArray(subMesh.VAO);
 
-    glGenBuffers(1, &subMesh.VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, subMesh.VBO);
-    glBufferData(GL_ARRAY_BUFFER, subMesh.Data.vertices.size() * sizeof(GLfloat), &subMesh.Data.vertices[0], GL_STATIC_DRAW);
-
     glGenBuffers(1, &subMesh.IBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, subMesh.IBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, subMesh.Data.indices.size() * sizeof(unsigned int), &subMesh.Data.indices[0], GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices[0]) * numOfIndices, indices, GL_STATIC_DRAW);
 
+    glGenBuffers(1, &subMesh.VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, subMesh.VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[0]) * numOfVertices, vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0]) * 3, 0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     glBindVertexArray(0);
 
@@ -49,7 +52,9 @@ void Mesh::RenderMesh()
     for (auto& subMesh : subMeshes)
     {
         glBindVertexArray(subMesh.VAO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, subMesh.IBO);
         glDrawElements(GL_TRIANGLES, subMesh.indexCount, GL_UNSIGNED_INT, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
     }
 }
@@ -58,9 +63,9 @@ void Mesh::ClearMesh()
 {
     for (auto& subMesh : subMeshes)
     {
-        glDeleteVertexArrays(1, &subMesh.VAO);
-        glDeleteBuffers(1, &subMesh.VBO);
         glDeleteBuffers(1, &subMesh.IBO);
+        glDeleteBuffers(1, &subMesh.VBO);
+        glDeleteVertexArrays(1, &subMesh.VAO);
     }
 
     subMeshes.clear();
