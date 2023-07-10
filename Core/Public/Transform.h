cc6975f282d23ad4aace53ab4796afd498bb16fa
Leandro Peres <leandroperes@protonmail.com>
2023-07-10 11:57:49 -0300

    Update Transform.h

--- Core/Public/Transform.h
+++ Core/Public/Transform.h
@@ -9,26 +9,27 @@
 
 namespace Vosgi
 {
-
     class Transform
     {
     public:
+        // Constructors
         Transform();
         Transform(glm::vec3 pos, glm::vec3 rot, glm::vec3 scale);
 
-        const glm::mat4 &GetModel() const { return m_model; }
+        // Getters
+        const glm::mat4& GetModel() const { return m_model; }
 
         // Position
-        const glm::vec3 &GetLocalPosition() const { return m_localPosition; }
+        const glm::vec3& GetLocalPosition() const { return m_localPosition; }
         glm::vec3 GetWorldPosition() const { return glm::vec3(m_model[3]); }
 
         // Rotation
-        const glm::vec3 &GetLocalRotation() const { return m_localEulerRotation; }
+        const glm::vec3& GetLocalRotation() const { return m_localEulerRotation; }
         glm::vec3 GetWorldRotation() const { return glm::vec3(glm::eulerAngles(glm::quat_cast(m_model))); }
 
         // Scale
-        const glm::vec3 &GetLocalScale() const { return m_localScale; }
-        glm::vec3 GetWorldScale() const { return {glm::length(GetRight()), glm::length(GetUp()), glm::length(GetBack())}; }
+        const glm::vec3& GetLocalScale() const { return m_localScale; }
+        glm::vec3 GetWorldScale() const { return { glm::length(GetRight()), glm::length(GetUp()), glm::length(GetBack()) }; }
 
         // Directions
         glm::vec3 GetForward() const { return glm::vec3(-m_model[2]); }
@@ -55,24 +56,32 @@ namespace Vosgi
             m_isDirty = true;
         }
 
+        // Dirty flag
         bool IsDirty() const { return m_isDirty; }
+        void SetDirty() { m_isDirty = true; }
 
+        // Model matrix computation
         void ComputeModelMatrix();
         void ComputeModelMatrix(glm::mat4 parentModel);
 
+        // Destructor
         ~Transform();
 
-    protected:
-        glm::vec3 m_localPosition = {0.0f, 0.0f, 0.0f};
-        glm::vec3 m_localEulerRotation = {0.0f, 0.0f, 0.0f};
-        glm::vec3 m_localScale = {1.0f, 1.0f, 1.0f};
+    public:
+        // Member variables
+        glm::vec3 m_localPosition = { 0.0f, 0.0f, 0.0f };
+        glm::vec3 m_localEulerRotation = { 0.0f, 0.0f, 0.0f };
+        glm::vec3 m_localScale = { 1.0f, 1.0f, 1.0f };
 
+    protected:
         glm::mat4 m_model = glm::mat4(1.0f);
 
         bool m_isDirty = true;
 
-    protected:
+    private:
+        // Helper function
         glm::mat4 GetLocalModelMatrix() const;
     };
 }
+
 #endif // !__TRANSFORM_H__
