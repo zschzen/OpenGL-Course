bb01311b234432b878b2b9b2a53eb3682451fafa
Leandro Peres <leandroperes@protonmail.com>
2023-07-18 17:41:58 -0300

    Create Rotating.h

---
+++ Core/Public/Rotating.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Behaviour.h"
+
+class Rotating : public Vosgi::Behaviour
+{
+public:
+    Rotating() : Behaviour() {}
+    ~Rotating() {}
+
+    // rotate the object around the Y-axis using quaternions angle-axis rotation
+    void Update(float deltaTime) override
+    {
+        glm::quat rotation = glm::angleAxis(glm::radians(speed * deltaTime), axis);
+        transform->SetRotation(transform->rotation * rotation);
+    }
+
+    float speed = 90.f;
+
+private:
+    glm::vec3 axis = glm::vec3(0.f, 1.f, 0.f);
+};
